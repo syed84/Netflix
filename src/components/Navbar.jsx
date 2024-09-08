899707184fe38b5ebdd76954f9d36e0ce2970dc1
Syed Arsalan <126832772+syed84@users.noreply.github.com>
2024-09-08 16:30:43 +0500

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,24 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
 
 const Navbar = () => {
-  
-
   return (
-    <div className='flex items-center justify-between p-4 z-[100] w-full absolute'>
-      <Link to='/'>
-        <h1 className='text-red-600 text-4xl font-bold cursor-pointer'>
+    <div className="flex items-center justify-between p-4 z-[100] w-full absolute">
+      <a href="/">
+        <h1 className="text-red-600 text-4xl font-bold cursor-pointer">
           NETFLIX
         </h1>
-      </Link>
-        <div>
-            <button className='text-white pr-4'>Sign In</button>        
-            <button className='bg-red-600 px-6 py-2 rounded cursor-pointer text-white'>
-              Sign Up
-            </button>
-        </div>
+      </a>
+
+      <div>
+        <a href="/login">
+          <button className="text-white pr-4">Sign In</button>
+        </a>
+        <a href="/signup">
+          <button className="bg-red-600 px-6 py-2 rounded cursor-pointer text-white">
+            Sign Up
+          </button>
+        </a>
+      </div>
     </div>
   );
 };
