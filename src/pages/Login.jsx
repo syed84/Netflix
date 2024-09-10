a1f9e4f893abe7740391ee3d23c9b67742f900d4
Syed Arsalan <126832772+syed84@users.noreply.github.com>
2024-09-10 06:39:12 +0500

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   const [email, setEmail] = useState("");
@@ -55,7 +56,7 @@ const Login = () => {
               </div>
               <p className="py-8">
                 <span className="text-gray-600">New to Netflix?</span>{" "}
-                <a href="/signup">Sign Up</a>
+                <Link to="/signup">Sign Up</Link>
               </p>
             </form>
           </div>
